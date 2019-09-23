47acbe8011ae853c91415d584dfa517a80e6ede2
Sandra Ossman <sandra.oss@lvjusd.k12.ca.us>
2019-09-23 13:21:00 -0700

    My Blue Jay Project Files

---
+++ ArraysandStuff.java
@@ -0,0 +1,51 @@
+
+/**
+ * Write a description of class ArraysandStuff here.
+ * 
+ * @author (your name) 
+ * @version (a version number or a date)
+ */
+public class ArraysandStuff
+{
+   public static void main(){
+    
+       
+       //random integer between 90 and 100 inclusive
+    int num;
+    
+    for (int i=0; i<30; i++){
+        num =(int)(Math.random()*11 +90);
+        System.out.println(num);
+        
+        System.out.println("remainder when divided by 2 is: " + num%5);
+    
+    }
+       
+       
+       
+       
+       
+    //int[] myArray = new int[8];
+    
+    //double num = Math.random();
+    //System.out.println(num);
+    
+   // for(int i = 0; i > 90; i++){
+        
+        //System.out.println(Math.random());
+   // }
+
+    
+    //System.out.println( myArray.length);
+       
+     //  for (int i=0; i < myArray.length; i++){
+         //  myArray[i]= i*15;
+          
+   // }
+    
+    // for (int i=0; i < myArray.length; i++){
+         //  System.out.println("The value is: " + myArray[i]);
+          
+    //}
+}
+}
